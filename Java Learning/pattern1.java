fb07e7e6e5a82dce6f1b087e5a821207281c1d2b
shubhamCodeHub <129199054+shubhamCodeHub@users.noreply.github.com>
2023-03-31 23:40:28 +0530

    Add files via upload

---
+++ Java Learning/pattern1.java
@@ -0,0 +1,28 @@
+/*
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+1 2 3 4 5 6 
+1 2 3 4 5 6 7 
+1 2 3 4 5 6 7 8 
+1 2 3 4 5 6 7 8 9 
+1 2 3 4 5 6 7 8 9 10 
+
+We can create the above pattern using the nested loop.
+ */
+
+class pattern1 {
+
+  public static void main(String[] args) {
+    int rows = 10;
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        // print displays the text without adding a new line
+        System.out.print(j + " ");
+      }
+      System.out.println(""); // println displays the text along with a new line
+    }
+  }
+}
