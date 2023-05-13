bafb4e0b213fbc8ef722c0f5544dec44c1a65a78
shubhamCodeHub <129199054+shubhamCodeHub@users.noreply.github.com>
2023-05-13 22:27:09 +0530

    Delete lecture_5_Hello_World.java

--- Spring Tutorial/Java Basic Tutorial/Section 1: Programming Core Java/lecture_5_Hello_World.java
+++
@@ -1,9 +0,0 @@
-public class lecture_5_Hello_World {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World! ");
-  }
-}
-/* NOTES :
- * main method is the starting point of a java program
- */
